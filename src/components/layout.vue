e9da5e7b23767ac7689cb11b376918946677b2c7
mazrommi <rokafordev@gmail.com>
2025-05-31 21:53:12 +0700

    menambah register

---
+++ src/components/layout.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div
+    class="bg-gradient-to-br from-gray-900 to-gray-800 min-h-screen flex items-center justify-center p-4">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter } from 'vue-router'
+import Layout from './components/layout.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+        component:Layout,
+        children: [
+            {
+                path: '/register',
+                component:
+        
+    },
+    {
+        path: '/dashboard',
+    }
+]})
 
 createApp(App).mount('#app')
